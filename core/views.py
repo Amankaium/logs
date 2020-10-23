30cde7901b3be791eb4e4d2820e775f4cbe64758
Amankaium <kaium@list.ru>
2020-10-23 22:00:24 +0600

    added cbv

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.generic import DetailView
+from django.views.generic import TemplateView
 from core.models import Article, Profile
 
 
@@ -23,6 +26,13 @@ def top(request):
     article = Article.objects.all().order_by("id").first()
     return render(request, "top.html", {"article": article})
 
+def user_fbv(request, id):
+    try:
+        user = User.objects.get(id=id)
+    except User.DoesNotExist:
+        raise Http404("Такого пользователя нет")
+    
+    return HttpResponse(f"Пользователь {user}, статей {user.created_article.count()}")
 
 def article(request, id):
     try:
@@ -40,6 +50,29 @@ def article(request, id):
         article_object.readers.add(user)
         article_object.save()
     return render(request, "article.html", {"article": article_object})
+
+
+class ArticleDetailView(DetailView):
+    template_name = "article_cbv.html"
+    queryset = Article.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        article = context["article"]
+        article.delete()
+        return redirect(homepage)
+    
+    def get(self, request, *args, **kwargs):
+        super(ArticleDetailView, self).get(request, *args, **kwargs)
+        context = self.get_context_data(**kwargs)
+        article_object = context["article"]
+        article_object.views += 1
+        user = request.user
+        if user.is_authenticated:
+            article_object.readers.add(user)
+            article_object.save()
+        return self.render_to_response(context)
+
 
 
 def profile(request, id):
@@ -80,3 +113,27 @@ def edit(request, id):
         return redirect("article", id=id)
 
     return render(request, "edit.html", {"article": article})
+
+
+# def another_test(request):
+#     a = User.objects.first()
+#     return render(request, "test_2.html", {"my_var": a})
+
+
+class FirstUserView(TemplateView):
+    template_name = "test_2.html"
+    
+
+    # def get(self, request, *args, **kwargs):
+    #     a = User.objects.first()
+    #     context = self.get_context_data(**kwargs)
+    #     context["my_var"] = a
+    #     return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        kwargs.setdefault('view', self)
+        kwargs["my_var"] = User.objects.first()
+        return kwargs
+
+
+    
--- logs/urls.py
+++ logs/urls.py
@@ -26,7 +26,10 @@ urlpatterns = [
     path("contacts/", contacts, name="contacts"),
     path("top/", top, name="top"),
     path("profile/<int:id>/", profile, name="profile"),
-    path("article/", include("core.urls"))
+    path("article/", include("core.urls")),
+    # path("test2/", another_test, name="test-2")
+    path("test2/", FirstUserView.as_view(), name="test-2-cbv"),
+    path("user/<id>/", user_fbv)
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
